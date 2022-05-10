bbc0a0f96264c69b32b9a59ef093bc85d174ca4a
Fan Yang <fanyang3@andrew.cmu.edu>
2022-05-10 01:17:32 -0400

    0099

--- scripts/seuss_sbatch_curriculum_single.sh
+++ scripts/seuss_sbatch_curriculum_single.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=2
 #SBATCH --time=600:00:00
-#SBATCH -o /home/fanyang3/github/safety-gym-curriculum/log/log_0095.out
-#SBATCH -e /home/fanyang3/github/safety-gym-curriculum/log/error_0095.out
+#SBATCH -o /home/fanyang3/github/safety-gym-curriculum/log/log_0099.out
+#SBATCH -e /home/fanyang3/github/safety-gym-curriculum/log/error_0099.out
 #SBATCH --partition=GPU 
-#SBATCH --exclude=compute-0-[7,9,11,13,19]
-#SBATCH --mem=4G
-#SBATCH --cpus-per-task=1
-#SBATCH --job-name='0095_safety_gym'
+#SBATCH --exclude=compute-0-[7,9,11]
+#SBATCH --mem=8G
+#SBATCH --cpus-per-task=2
+#SBATCH --job-name='0099_safety_gym'
 # module load cuda-91 
 module load singularity
 
@@ -17,8 +17,8 @@ singularity exec  \
   'source ~/.bashrc &&  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/fanyang3/.mujoco/mujoco200/bin && conda activate safety_gym  && \
  	python experiment.py \
   --seed='${1}' --exp_name='${2}' \
-  --cpu=1 --robot=point --task=goal1 --algo=ppo_lagrangian --curriculum --init_cost_lim=512 --target_cost_lim=25 \
-  --decrease_ratio=0.5 --stable_length=10 --penalty_lr='${3}' --clip_penalty' &
+  --cpu=2 --robot=point --task=push2 --algo=ppo_lagrangian --curriculum --init_cost_lim=512 --target_cost_lim=25 \
+  --decrease_ratio=0.5 --stable_length=10 --penalty_lr='${3}'' &
 
 wait
 
